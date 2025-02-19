86bbf3efc41da86f251479338fb9016ceddbe252
Peter Muhumuki <muhumukip@gmail.com>
2025-02-19 11:11:14 +0300

    implement RESTful API for drinks with list and detail views

--- drinksdata/urls.py
+++ drinksdata/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import drink_list
+from .views import *
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     
     path('', drink_list, name = 'drinks-list'),
+    path("<int:id>", drink_detail, name = "single-drink"),
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- drinksdata/views.py
+++ drinksdata/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models  import Drinks
-from django.views import View
 from .serializers import DrinkSerializer
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
-def drink_list(request):
-    drinks = Drinks.objects.all()
-    serializer = DrinkSerializer(drinks, many=True).data
+@api_view(['GET', 'POST'])
+def drink_list(request, format = None):
+    if request.method == 'GET':
+        drinks = Drinks.objects.all()
+        serializer = DrinkSerializer(drinks, many=True).data
+        return Response(serializer)
 
-    return JsonResponse({"drinks": serializer})
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id, format = None):
+    try:
+        drink = Drinks.objects.get(id = id)
+    except Drinks.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.erros, status = status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
